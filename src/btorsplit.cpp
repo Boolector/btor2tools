0a55a9e446def88d1b8489c08225bde385833afc
Mathias Preiner <mathias.preiner@gmail.com>
2019-10-16 15:59:21 -0700

    Add missing copyright headers.

--- src/btorsplit.cpp
+++ src/btorsplit.cpp
@@ -1,3 +1,14 @@
+/**
+ *  Btor2Tools: A tool package for the BTOR format.
+ *
+ *  Copyright (c) 2019 Mathias Preiner.
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of the Btor2Tools package.
+ *  See LICENSE.txt for more information on using this software.
+ */
+
 #include <sys/stat.h>
 #include <algorithm>
 #include <cstdarg>
