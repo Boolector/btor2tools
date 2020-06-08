0e55a8c3a3ea5f27c6dc33daeebb73f7617032d0
N. Engelhardt <nak@symbioticeda.com>
2020-06-08 19:59:49 +0200

    remove frame checks

--- src/btorsim/btorsim.cpp
+++ src/btorsim/btorsim.cpp
@@ -1466,7 +1466,6 @@ parse_sat_witness ()
   int64_t k = 0;
   while (parse_frame (k)) k++;
 
-  // if (!found_initial_frame && states.size() > 0) parse_error ("initial frame missing");
   msg (1, "finished parsing k = %" PRId64 " frames", k);
   if (dump_vcd) vcd_writer->update_time(k+1);
 
@@ -1492,8 +1491,6 @@ parse_unknown_witness ()
   int64_t k = 0;
 
   while (parse_frame (k)) k++;
-
-  // if (!found_initial_frame && states.size() > 0) parse_error ("initial frame missing");
 
   report ();
   if (print_trace) printf (".\n"), fflush (stdout);
